c63688d57573da09f9976f564203a3f3da6d5795
David Nies <nies.david@googlemail.com>
2015-07-29 01:58:12 +0200

    Finish kafka Dockerfile

---
+++ docker/images/kafka/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i "s/^zookeeper\.connect=.*$/zookeeper.connect=${ZOO_CONNECTION}/" /opt/kafka/config/server.properties
+
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties | tee -a /data/logs/output.log
