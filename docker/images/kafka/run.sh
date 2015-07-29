f7d7d42c6718e9dd156e0b23e71976d7ec5448b4
David Nies <nies.david@googlemail.com>
2015-07-29 17:21:31 +0200

    Let /data/logs be created by run.sh for kafka image

--- docker/images/kafka/run.sh
+++ docker/images/kafka/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /data/logs
+
 sed -i "s/^zookeeper\.connect=.*$/zookeeper.connect=${ZOO_CONNECTION}/" /opt/kafka/config/server.properties
 
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties | tee -a /data/logs/output.log
